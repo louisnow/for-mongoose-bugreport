7a64d0efb027c41580508382312921e7ecbe5052
Louis Christopher <louis9171@outlook.com>
2020-02-18 12:05:39 +0530

    Fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Person = require("./models/Person");
     p1: { name: "Steve" },
     pmany: [{ name: "Harry" }]
   }).save();
-  const cre = await Person.findOne({ "person.name": "Steve" }).exec();
+  const cre = await Person.findOne({ "p1.name": "Steve" }).exec();
   const updatedObj = { p1: { name: "Smith" }, pmany: [{ name: "Potter" }] };
   Object.assign(cre, updatedObj);
   await cre.save();
